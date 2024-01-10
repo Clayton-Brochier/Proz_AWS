6bf6fcf00d27237fbf783b620562046228d3d44b
Clayton Brochier <claytonbrochier@hotmail.com>
2024-01-10 17:20:03 -0300

    Desafio incluído

---
+++ desafio.py
@@ -0,0 +1,20 @@
+'''DESAFIO 
+
+Instruções do projeto:
+
+Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal usando o comando print(). 
+O primeiro array deve conter os produtos de uma loja da sua escolha (loja de comida, materiais de construção, música, etc). 
+O segundo array deve conter os anos de nascimento de familiares e amigos seus. Lembre-se de usar nomes descritivos para nomear cada variável, 
+e de usar o tipo de dado apropriado para cada lista (strings, booleanos, números inteiros, floats).'''
+
+#Declarar dois arrays com mínimo de 5 elementos cada;
+itens_casa_de_carnes = ['Sal grosso', 'Picanha', 'Carvão', 'Vazio', 'Maminha']
+
+ano_nascimento_familiares = [1985, 1989, 2009, 2013]
+
+#Imprimir na tela usando o comando print();
+
+print(itens_casa_de_carnes)
+
+print(ano_nascimento_familiares)
+
