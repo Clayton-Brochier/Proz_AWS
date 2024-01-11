c2deb50a8d3ec2e7bbaab5dcf4da834b3be0b8a7
ClaytonBrochier <claytonbrochier@hotmail.com>
2024-01-11 16:03:55 -0300

    Acrescentado novo desafio sobre indices

--- Indices/desafioIndice.py
+++ Indices/desafioIndice.py
@@ -17,3 +17,19 @@ print(lista_musicos[9])
 
 #imprima no terminal: o dado salvo no índice 14
 print(lista_musicos[14])
+
+
+'''Uma nova loja de cosméticos abriu no seu bairro e pediram para você elaborar um sistema que imprime na tela na frente da loja os novos 
+produtos que chegaram. O sistema da loja já tem um array com os produtos, você precisa apenas imprimir eles no terminal, um por um.
+
+Como desafio opcional, tente imprimir cada produto com a frase "Temos [produto] à venda!" (ex. "Temos máscaras faciais à venda!"). 
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+'''
+
+#elaborar um sistema que imprime na tela na frente da loja os novos produtos que chegaram um por um.
+
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores']
+for i in range(len(lista_produtos)):
+    print("Temos {} à venda!".format(lista_produtos[i]))
+
+#https://colab.research.google.com/drive/1Oc-Bkhhunm4TnpUdxdBHZPXusTzaNaL-?usp=sharing
